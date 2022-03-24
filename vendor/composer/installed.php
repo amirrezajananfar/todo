5d331beeba17e217e78a0f40aa8128e1a222e9b5
Amirreza Jananfar <amirrezajananfar@gmail.com>
2022-03-24 23:36:34 +0430

    Made some changes about composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'c2e764ab607206c076dacdc640f188432a7c1b2b',
+        'name' => 'amirreza/todo',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'amirreza/todo' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'c2e764ab607206c076dacdc640f188432a7c1b2b',
+            'dev_requirement' => false,
+        ),
+    ),
+);
